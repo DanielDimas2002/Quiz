15b6ed9c33a126d8aa0ec5574f87eca4af2ac22c
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-02-05 10:53:21 -0300

    CSS básico

--- Quiz/src/components/Welcome.jsx
+++ Quiz/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import Quiz from "../img/quiz.svg"
 const Welcome = () => {
 
     return(
-        <div>
+        <div id="welcome">
             <h2>Seja bem-vindo</h2>
             <p>Clique no botão abaixo para começar:</p>
             <button>Iniciar</button>
