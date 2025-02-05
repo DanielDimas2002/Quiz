3d20d8e6ca99379796fe727f98eacf2af91e8b87
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-02-05 11:35:00 -0300

    Criação do Contexto

--- Quiz/src/components/Welcome.jsx
+++ Quiz/src/components/Welcome.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
 import "./Welcome.css"
 import Quiz from "../img/quiz.svg"
 const Welcome = () => {
+
+    const quizState = useContext(QuizContext);
+    console.log(quizState)
 
     return(
         <div id="welcome">
