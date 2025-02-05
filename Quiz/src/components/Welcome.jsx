ad50b0dd07ea4171ae20b3087ad22662f1fc4424
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-02-05 15:20:29 -0300

    Controle de Estados

--- Quiz/src/components/Welcome.jsx
+++ Quiz/src/components/Welcome.jsx
@@ -1,18 +1,18 @@
-import { useContext } from "react";
+ import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 import "./Welcome.css"
 import Quiz from "../img/quiz.svg"
+
 const Welcome = () => {
 
-    const quizState = useContext(QuizContext);
-    console.log(quizState)
+    const [quizState, dispatch] = useContext(QuizContext)
 
     return(
         <div id="welcome">
             <h2>Seja bem-vindo</h2>
             <p>Clique no botão abaixo para começar:</p>
-            <button>Iniciar</button>
-            <img src={Quiz} alt = ""/>
+            <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
+            <img src={Quiz} alt = "Inicio do Quiz"/>
         </div>
     );
 
