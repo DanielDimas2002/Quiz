739e4ad5f4ddb985786be3c36c4b20e31a09f321
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-02-05 10:00:39 -0300

    HTML inicial

---
+++ Quiz/src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import "./Welcome.css"
+import Quiz from "../img/quiz.svg"
+const Welcome = () => {
+
+    return(
+        <div>
+            <h2>Seja bem-vindo</h2>
+            <p>Clique no botão abaixo para começar:</p>
+            <button>Iniciar</button>
+            <img src={Quiz} alt = ""/>
+        </div>
+    );
+
+}; export default Welcome
---
+++ Quiz/src/data/questions.js
@@ -0,0 +1,26 @@
+const perguntasTeste = [
+    {
+      category: "HTML",
+      pergunta: "Qual tag usamos para criar um link em HTML?",
+      respostas: ["<link>", "<a>", "<href>"],
+      correta: "<a>",
+      dica: "É a mesma tag usada para âncoras."
+    },
+    {
+      category: "CSS",
+      pergunta: "Qual propriedade CSS altera a cor do texto?",
+      respostas: ["font-color", "text-color", "color"],
+      correta: "color",
+      dica: "É uma palavra simples e direta."
+    },
+    {
+      category: "JavaScript",
+      pergunta: "Qual método do JavaScript exibe uma mensagem na tela?",
+      respostas: ["console.log()", "alert()", "prompt()"],
+      correta: "alert()",
+      dica: "É uma função usada para alertar o usuário."
+    }
+  ];
+  
+  export default perguntasTeste;
+  
