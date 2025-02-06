4f4bdc46b33e76cd659f4f6a4fa66ae10e061702
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-02-06 11:17:43 -0300

    Tela de GameOver

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,7 +1,9 @@
 import { useContext, useEffect } from 'react'
+import { QuizContext } from './context/quiz'
 import Welcome from './components/Welcome'
 import Question from './components/Question'
-import { QuizContext } from './context/quiz'
+import GameOver from './components/GameOver'
+
 import './App.css'
 
 
@@ -18,6 +20,7 @@ const [quizState, dispatch] = useContext(QuizContext);
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome/>}
       {quizState.gameStage === "Playing" && <Question/>}
+      {quizState.gameStage === "End" && <GameOver/>}
    </div>
   )
 }
