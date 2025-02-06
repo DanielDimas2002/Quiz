95b39e8a1df0efe79aa2509f36954b6338239d28
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-02-06 11:34:59 -0300

    Tela gameover

--- Quiz/src/components/GameOver.jsx
+++ Quiz/src/components/GameOver.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import { useContext } from "react"
+import { QuizContext } from "../context/quiz"
+import GG from "../img/GameOver.svg"
+
+import "./GameOver.css"
 
 const GamerOver = () =>{
     return(
-        <div>
-            <h2>Game Over</h2>
+        <div id="gameover">
+            <h2>Fim de Jogo!</h2>
+            <p>Pontuação: x</p>
+            <p>Você acertou y de z perguntas.</p>
+            <img src={GG} alt="Fim do Quiz"/>
+            <button>Reiniciar</button>
         </div>
     )
 }
